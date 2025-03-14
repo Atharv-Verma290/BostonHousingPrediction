16e1bba1753d47fef81a471176af76eb18acb988
AtharvVerma-290 <atharv.verma29k@gmail.com>
2025-03-14 08:42:49 +0530

    basic_data_inspection.py

--- boston-housing-prediction/boston_housing_prediction/ingest_data.py
+++ boston-housing-prediction/boston_housing_prediction/ingest_data.py
@@ -59,17 +59,18 @@ class DataIngestorFactory:
 
 # Example Usage:
 if __name__ == "__main__":
-    # Specify the file path
-    file_path = "/Users/atharv/MLProjects/BostonHousingPrediction/boston-housing-prediction/data/archive.zip"
+    # # Specify the file path
+    # file_path = "/Users/atharv/MLProjects/BostonHousingPrediction/boston-housing-prediction/data/archive.zip"
 
-    # Determine the file extension
-    file_extension = os.path.splitext(file_path)[1]
+    # # Determine the file extension
+    # file_extension = os.path.splitext(file_path)[1]
 
-    # Get the appropriate DataIngestor
-    data_ingestor = DataIngestorFactory.get_data_ingestor(file_extension)
+    # # Get the appropriate DataIngestor
+    # data_ingestor = DataIngestorFactory.get_data_ingestor(file_extension)
 
-    # Ingest the data and load it into a DataFrame
-    df = data_ingestor.ingest(file_path)
+    # # Ingest the data and load it into a DataFrame
+    # df = data_ingestor.ingest(file_path)
 
-    # Now df contains the DataFrame from the extracted CSV
-    print(df.head()) 
+    # # Now df contains the DataFrame from the extracted CSV
+    # print(df.head()) 
+    pass
